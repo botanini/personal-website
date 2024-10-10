766c7d504b0c02c62b8135d02be44ca862f26705
Lily Yang <109325333+llyng@users.noreply.github.com>
2024-10-09 22:03:52 -0400

    finished about me section and the tab buttons

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -2,10 +2,44 @@
 import React, { useTransition, useState } from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>C</li>
+        <li>Python</li>
+        <li>PostgreSQL</li>
+        <li>Java</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+    )
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li> Rutgers University, New Brunswick, NJ </li>
+      </ul>
+    )
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Working on AWS Cloud Practitioner</li>
+      </ul>
+    )
+  }
+];
 
 const AboutSection = () => {
   const [tab, setTab] = useState("skills");
-  const [startTransition, isPending] = useTransition();
+  const [isPending, startTransition] = useTransition();
 
   const handleTabChange = (id) => {
     startTransition(() => {
@@ -14,7 +48,7 @@ const AboutSection = () => {
   };
 
   return (
-    <section className="text-white">
+    <section className="text-[#070042]" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image
           src="/images/aboutme1.svg"
@@ -22,9 +56,9 @@ const AboutSection = () => {
           height={500}
           alt="About Me"
         />
-        <div>
-          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-base md:text-lg">
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-[#070042] mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
             Hi, I'm Lily! I'm a junior at Rutgers University, where I'm pursuing
             a degree in Computer Science and Information Technology. I'm
             passionate about exploring new technologies and combining my love
@@ -35,23 +69,35 @@ const AboutSection = () => {
             experimenting with new tools, I'm always excited to learn and push
             my skills further.
           </p>
-          <div className="flex flex-row mt-8">
+          <div className="flex flex-row justify-start mt-8">
             <TabButton
               selectTab={() => handleTabChange("skills")}
-              active={"skills"}
+              active={tab === "skills"}
             >
-              Skills
+              {" "}
+              Skills{" "}
             </TabButton>
-            <span className="mr-3 font-semibold hover:text-white text-[#070042] border-b border-blue-950">
-              Skills
-            </span>
-            <span className="text-2xl font-bold">Education</span>
-            <span className="text-2xl font-bold">Experience</span>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
           </div>
         </div>
       </div>
     </section>
   );
 };
-
 export default AboutSection;
--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 
 const TabButton = ({ active, selectTab, children }) => {
-  const buttonClasses = active
-    ? "text-white border-b border-blue-950"
-    : "hover:text-white text-[#070042]";
+  const buttonClasses = active ? "text-[#070042]" : "text-[#474aa6]";
   return (
     <button onClick={selectTab}>
-      <p className={"mr-3 font-semibold hover:text-white ${buttonClasses}"}>
+      <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
         {children}
       </p>
     </button>
