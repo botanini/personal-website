ae38ce3ffc8dbc5ad3dd50ce11d256f0654b5c7b
Lily Yang <109325333+llyng@users.noreply.github.com>
2024-11-13 12:31:38 -0500

    project section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
-import Navbar from "./components/Navbar"
-import AboutSection from "./components/AboutSection"
-
+import Navbar from "./components/Navbar";
+import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#79AEFF]">
-      <Navbar/>
+      <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
-        <HeroSection/>
-        <AboutSection/>
+        <HeroSection />
+        <AboutSection />
       </div>
-
     </main>
   );
 }
